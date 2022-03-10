bed5aa069175210446df3b594d07a0ebe96216e9
madhu101298 <100665812+madhu101298@users.noreply.github.com>
2022-03-10 11:49:41 +0530

    Update jenkins_jfrog_intigration.groovy

--- jenkins_jfrog_intigration.groovy
+++ jenkins_jfrog_intigration.groovy
@@ -1,1 +1,29 @@
 // this is jfrog jenkins intigration
+node {
+    def server
+    def rtMaven = Artifactory.newMavenBuild()
+    def buildInfo
+    
+    stage('Clone') {
+        git url: 'https://github.com/madhu101298/project-examples.git'
+    }
+    
+        stage ('Artifactory configaration') {
+        // Obtain an Artifactory server instance, defined in Jenkins --> Manage Jenkins --> Configure System:
+        Server = Artifactory.server 'JFROG_ARTIFACTORY'
+        
+        //Tool name from jenkins Configuration
+        rtMaven.tool = 'M3'
+        rtMaven.deployer releaseRepo: 'jenkins-int', snapshotRepo: 'jenkins-int', server:server
+        rtMaven.resolver releaseRepo: 'madhu-maven', snapshotRepo: 'madhu-maven', server:server
+        buildInfo = Artifactory.newBuildInfo()
+    }
+    
+    stage ('Exec Maven'){
+        rtMaven.run pom: 'maven-examples/maven-example/pom.xml', goals:'clean install', buildInfo:buildInfo
+    }
+    
+    stage ('Publish build Info') {
+        server publishBuildInfo buildInfo
+    }
+}    
